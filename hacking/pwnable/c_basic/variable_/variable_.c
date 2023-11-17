b2717892f9b72118498dd10bbe020d9a597dc975
accio3014 <accio3014@gmail.com>
2023-11-17 21:38:06 +0900

    Update c

--- hacking/pwnable/c_basic/variable_/variable_.c
+++ hacking/pwnable/c_basic/variable_/variable_.c
@@ -18,15 +18,32 @@ int main() {
     // float    %f
     // %c, %s, %d, %f는 포맷팅 기호로 따음표 안에 들어가며 포맷팅 기호 자리에 variable의 값이 들어감
 
-    char name = 'J';            // char의 경우 문자 1개를 저장함
-    char name_2[6] = "Jeong";   // 문자열을 저장하고 싶은 경우에는 variable[문자열의길이 + 1]로 저장 가능
-    int age = 10;
-    float pi = 3.14;
+    // char name = 'J';            // char의 경우 문자 1개를 저장함
+    // char name_2[6] = "Jeong";   // 문자열을 저장하고 싶은 경우에는 variable[문자열의길이 + 1]로 저장 가능
+    // int age = 10;
+    // float pi = 3.14;
 
-    printf("%c\n", name);       // name variable에는 문자 1개가 저장되었기 때문에 %c
-    printf("%s\n", name_2);     // name_2 variable에는 문자열이 저장되어 있기 때문에 %s
-    printf("%d %f\n", age, pi);
+    // printf("%c\n", name);       // name variable에는 문자 1개가 저장되었기 때문에 %c
+    // printf("%s\n", name_2);     // name_2 variable에는 문자열이 저장되어 있기 때문에 %s
+    // printf("%d %f\n", age, pi);
 
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    // Keyboard typing(= input)
+    // C언어에서는 키보드 입력을 받기 위해 scanf()함수를 사용
+    // scanf("%d", &variable);
+    // 위 형식으로, 포맷팅 기호와 같이 사용함
+
+    int i = 0;              // 키보드 입력을 저장할 변수 초기화(Initializaion)
+    scanf("%d", &i);        // %d 이기 때문에 정수를 입력 받음
+    printf("%d\n", i);
+
+    char anything[101] = "";
+    printf("최대 100자리의 문자를 입력하세요 : ");
+    scanf("%s", anything);          // 문자열의 경우 &는 사용 안함
+    printf("%s \n", anything);
 
 
     return 0;
