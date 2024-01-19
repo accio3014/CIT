adb5589dbe90254ea4c5e58afe2af3e23ec35660
accio3014 <devaccio2023@gmail.com>
2024-01-19 20:33:50 +0900

    Update c

--- hacking/pwnable/c_basic/pointer/pointer_.c
+++ hacking/pwnable/c_basic/pointer/pointer_.c
@@ -5,8 +5,8 @@ int main() {
     // pointer
     // pointer는 주소에 대한 개념
     // pointer variable는 일반적인 variable과 다르게 address를 저장
-    // int *p = variable;
-    // 위와 같은 형태로 사용
+    // int *p = &variable;
+    // 위와 같은 형태로 사용, variable name 앞에 *이 있으면 pointer variable
 
     // int a = 10;
     // int *p = &a;            // a의 address를 저장
