f1c3a08a51f3fe2cf888c7ea5e328503af446fc1
accio3014 <accio3014@gmail.com>
2024-03-31 19:09:39 +0900

    python basic update

---
+++ python/baekjoon.py
@@ -0,0 +1,5 @@
+gpaScore = {"A+":4.5, "A0":4.0, "B+":4.5, "B-":4.0, "C+":4.5, "C-":4.0, "D+":4.5, "D0":4.0, "F":4.5}
+subject, credits, gpa = input().split(' ')
+print(subject)
+print(credits)
+print(gpa)
